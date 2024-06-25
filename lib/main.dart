947675563ba22fd99961ff3d6441dab58763a2b8
Karim Omar <okareem250@gmail.com>
2024-06-25 07:41:49 +0300

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             fontFamily: 'Cairo',
           ),
-          home: const HomeView()),
+          home: const OnboardingView()),
     );
   }
 }
