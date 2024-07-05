fe775983d8cc006c466732d913ed8cb685c8abe8
Karim Omar <okareem250@gmail.com>
2024-07-05 18:24:58 +0300

    notifcations task done

---
+++ lib/core/networking/firebase_notifcations.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:selaty_app/features/notification_test/test.dart';
+import 'package:selaty_app/main.dart';
+import 'package:short_navigation/short_navigation.dart';
+
+class FirebaseNotifcations {
+  /// 1 -  creat instance of FBM
+  final _firebaseMessaging = FirebaseMessaging.instance;
+
+// A-  create func to init the notications
+  Future<void> initNotications() async {
+    // take the permission
+    await _firebaseMessaging.requestPermission();
+    // take the token -- every device has its own tkoen
+    String? token = await _firebaseMessaging.getToken();
+    print("Token: $token");
+    handelBackgroundNotification();
+
+    // >> go to main and call this func
+  }
+
+  // B- create f func to handele the notication
+
+  void hendelMessage(RemoteMessage? message) {
+    if (message == null) return;
+    Go.toName(Test.routName, arguments: message);
+    // Go.navigatorKey.currentState!.pushNamed("test", arguments: message);
+    // we passed the message to the view we want to get in when we tap in the notificaton to view the notifaction details
+// then go create this key in the main file
+// final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  }
+
+  // C- handel the background notifications to get notifications even the app is trimenated
+
+  Future handelBackgroundNotification() async {
+    FirebaseMessaging.instance.getInitialMessage().then(hendelMessage);
+
+    FirebaseMessaging.onMessageOpenedApp.listen(hendelMessage);
+    // then add onMessageOpenedApp to the initNotifications func above
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:selaty_app/core/networking/firebase_notifcations.dart';
+import 'package:selaty_app/features/notification_test/test.dart';
 import 'package:selaty_app/features/onboarding/presentation/onboarding_view.dart';
+import 'package:selaty_app/firebase_options.dart';
 import 'package:short_navigation/short_navigation.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Future.delayed(const Duration(seconds: 3));
   FlutterNativeSplash.remove();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseNotifcations().initNotications();
+
   runApp(DevicePreview(builder: (context) => const MyApp()));
 }
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,6 +31,7 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       splitScreenMode: true,
       child: MaterialApp(
+          routes: {"test": (context) => Test()},
           locale: DevicePreview.locale(context),
           navigatorKey: Go.navigatorKey,
           debugShowCheckedModeBanner: false,
